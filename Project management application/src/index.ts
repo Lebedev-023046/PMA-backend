be3b6c652108dec4b3daf4ce6c8e8878451031d1
lebedev-023046 <lebedev.023046@gmail.com>
2022-11-11 20:34:56 +0300

    feat: add backend for PMA

---
+++ Project management application/src/index.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import { PORT } from "./constants";
+
+import * as serverService from "./services/server.service";
+
+(async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://swan_023046:S7vXU4.VjGtKdV7@cluster.c6q1rzl.mongodb.net/?retryWrites=true&w=majority"
+    );
+    serverService.server.listen(process.env.PORT || PORT, function () {
+      console.log("Сервер ожидает подключения...");
+    });
+  } catch (error) {
+    console.log(error);
+  }
+})();
+
+process.on("SIGINT", async () => {
+  await mongoose.disconnect();
+  process.exit();
+});
